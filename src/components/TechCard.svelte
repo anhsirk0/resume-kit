77f2c3a64f8d3a7802a60cc7de566537d78029e0
krishna <krishna404@yandex.com>
2025-01-12 12:33:09 +0530

    update stuff

--- src/components/TechCard.svelte
+++ src/components/TechCard.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div in:fade={{ delay }} class="card">
-  <div class="card-body p-4 lg:p-6 2xl:p-8">
+  <div class="card-body p-0">
     <div class="text-xl md:text-2xl 2xl:text-4xl mb-3 xl:mb-4 font-bold">
       <p class="w-fit relative isolate">
         <span class="absolute -bottom-1 left-0 w-[120%] h-4 bg-base-300 -z-10"
@@ -22,7 +22,7 @@
           data-tip={item.title}
         >
           <div
-            class="size-20 lg:size-24 xl:size-28 flex items-center border-1 border-base-content/25 rounded-box p-1 md:p-2"
+            class="size-20 xl:size-24 2xl:size-28 flex items-center border-1 border-base-content/25 rounded-box p-1 md:p-2"
           >
             <img alt={item.title} src={"/" + item.img} class="rounded-2xl" />
           </div>
